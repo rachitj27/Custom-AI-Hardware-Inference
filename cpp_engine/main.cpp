14f84df3880d57240e0daad5bccaf5a2a951e1bc
rachitj27 <rjrachit06@gmail.com>
2026-07-06 15:27:10 -0700

    C++ project scaffolding with CMake

---
+++ cpp_engine/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Helios C++ engine skeleton" << std::endl;
+    std::cout << "Ready to load model_int8.bin and run inference" << std::endl;
+    return 0;
+}
